2c6eb81f15f408c91f80725dc8bf8b2c78d9baf7
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-10-19 23:38:53 -0500

    feat: Define Product and Review interfaces for product data structure

--- src/app/types/product.ts
+++ src/app/types/product.ts
@@ -1,8 +1,15 @@
+import { Review } from "./review";
+
 export interface Product {
   id: number;
   title: string;
-  image: string;
+  description: string;
+  thumbnail: string;
+  discountPercentage: number;
+  rating: number;
   price: number;
   stock?: number;
+  brand?: string;
   quantity?: number;
+  reviews: Review[];
 }
