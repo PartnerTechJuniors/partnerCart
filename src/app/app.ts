e7084dfaa1931b71c034f0e0e43ebbeccb2a2bbe
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-10-19 23:41:38 -0500

    refactor: Remove unused ProductsList import and styleUrl from App component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductsList } from './pages/products-list/products-list';
 import { Header } from './sections/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductsList, Header],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, Header],
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('cart');
