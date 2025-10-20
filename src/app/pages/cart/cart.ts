abf07371391eae13c2e48800f0448dfa523cf654
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-10-19 23:40:36 -0500

    feat: Update cart component with localized titles and improved layout

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -2,13 +2,17 @@ import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart';
 import { CartItem } from "./cart-item/cart-item";
 import { RouterLink } from "@angular/router";
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-cart',
   imports: [CartItem, RouterLink],
-  templateUrl: './cart.html',
-  styleUrl: './cart.css'
+  templateUrl: './cart.html'
 })
 export class Cart {
   cartService = inject(CartService);
+
+  constructor(private titleService: Title){
+    this.titleService.setTitle(`Mi carrito - ${this.cartService.cartitems().length} articulos`);
+  }
 }
