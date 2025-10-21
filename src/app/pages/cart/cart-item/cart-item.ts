2365f1f53e215f412d3355db011a497ae9512e48
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-10-21 13:59:07 -0500

    improve style and info

--- src/app/pages/cart/cart-item/cart-item.ts
+++ src/app/pages/cart/cart-item/cart-item.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 import { CartService } from '@services/cart';
 import { Button } from '@components/button/button';
 import { Product } from '@app/types/product';
+import { Icontrash } from '@icons/icontrash/icontrash';
+import { QuantitySelector } from "@componentsquantity-selector/quantity-selector";
 
 @Component({
   selector: 'app-cart-item',
-  imports: [CommonModule, Button],
+  imports: [CommonModule, Button, Icontrash, QuantitySelector],
   templateUrl: './cart-item.html'
 })
 export class CartItem {
-  cartService = inject(CartService)
+  cartService = inject(CartService);
   item = input.required<Product>();
+  
+  priceWithDiscount(){
+    return this.item().price * (1 - this.item().discountPercentage / 100);
+  }
+
+  discountPercentageRound(){
+    return Math.round(this.item().discountPercentage);
+  }
 }
