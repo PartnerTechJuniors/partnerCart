aa007baee15cc0c8c4ff03e3b4a6fa9b626a4293
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-10-22 03:00:36 -0500

    style: add componente icon cart

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -5,10 +5,11 @@ import { RouterLink } from "@angular/router";
 import { Title } from '@angular/platform-browser';
 import { CurrencyPipe, Location } from '@angular/common';
 import { PrimaryButton } from "@componentsprimary-button/primary-button";
+import { Iconcart } from "@componentsicons/iconcart/iconcart";
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItem, RouterLink, CurrencyPipe, PrimaryButton],
+  imports: [CartItem, RouterLink, CurrencyPipe, PrimaryButton, Iconcart],
   templateUrl: './cart.html'
 })
 export class Cart {
