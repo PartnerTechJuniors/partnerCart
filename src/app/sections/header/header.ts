7a20c6ff5b7d6c6b85f2fd3c01424e863f6f07a0
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-10-23 01:00:04 -0500

    feat: add icon cart in to button header

--- src/app/sections/header/header.ts
+++ src/app/sections/header/header.ts
@@ -2,13 +2,15 @@ import { Component, inject, signal } from '@angular/core';
 import { PrimaryButton } from "@components/primary-button/primary-button";
 import { CartService } from '@services/cart';
 import { RouterLink } from "@angular/router";
+import { LucideAngularModule, ShoppingCartIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [PrimaryButton, RouterLink],
+  imports: [PrimaryButton, RouterLink, LucideAngularModule],
   templateUrl: './header.html'
 })
 export class Header {
   cartService = inject(CartService);
   nameShop = signal('PartnerCart');
+  readonly ShoppingCartIcon = ShoppingCartIcon;
 }
