d778c94ff9e48abb617332788602373b67d7cc69
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-10-23 00:59:10 -0500

    feat: add component into app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Toast } from '@components/toast/toast';
 import { Header } from '@sections/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Toast],
   templateUrl: './app.html'
 })
 export class App {
