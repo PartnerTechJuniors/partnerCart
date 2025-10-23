4ef364c99de58d271c07af616203ad5613340fd4
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-10-23 00:58:37 -0500

    feat: add count up fo total prices

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -3,19 +3,29 @@ import { CartService } from '@services/cart';
 import { CartItem } from "./cart-item/cart-item";
 import { RouterLink } from "@angular/router";
 import { Title } from '@angular/platform-browser';
-import { CurrencyPipe, Location } from '@angular/common';
+import { Location } from '@angular/common';
 import { PrimaryButton } from "@componentsprimary-button/primary-button";
 import { Iconcart } from "@componentsicons/iconcart/iconcart";
+import { CountUpModule } from 'ngx-countup';
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItem, RouterLink, CurrencyPipe, PrimaryButton, Iconcart],
+  imports: [CartItem, RouterLink, PrimaryButton, Iconcart, CountUpModule],
   templateUrl: './cart.html'
 })
 export class Cart {
   cartService = inject(CartService);
   location = inject(Location);
   
+  countUpOptions = {
+    prefix: 'S/ ',
+    decimalPlaces: 2,
+    separator: ',',
+    decimal: '.',
+    useEasing: true,
+    duration: 2
+  };
+
   constructor(private titleService: Title){
     this.titleService.setTitle(`Mi carrito - ${this.cartService.countQuantityProduct()} articulos`);
   }
